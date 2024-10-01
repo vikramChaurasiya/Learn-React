96de3aa193591748fce97ae6a8aa8832a12acd98
vikramChaurasiya <bhartivikram633@gmail.com>
2024-10-01 11:40:54 +0530

    Basic react

---
+++ 01_Basic_React/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello(){
+    return <>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+    </>
+}
+
+export default Hello;
