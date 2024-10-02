cf962290d2e2ec9a0f54d048808d161aefe04ac1
vikramChaurasiya <bhartivikram633@gmail.com>
2024-10-02 19:32:36 +0530

    basic react

--- 01_Basic_React/src/App.jsx
+++ 01_Basic_React/src/App.jsx
@@ -1,11 +1,14 @@
 // import './App.css';
-import Hello from './hello.jsx';
+import Hello from './Hello';
 
 function App() {
+  // let skill = [<li>"HTML"</li>,<li>"CSS"</li>,<li>"JS"</li>];
+  let skill = ["html" ,"css" , "js"]
+  let skill2 = {H:"HTML", C:"CSS" ,J:"JS" } 
   return (
     <>
-     <h1>hello world</h1>
-     <Hello/>
+     {/*  <h1>hello world</h1> */}
+     <Hello name="vikram" place="dbg" skills = {skill} skills2 = {skill2.H} />
     </>
   )
 }
--- 01_Basic_React/src/Hello.jsx
+++ 01_Basic_React/src/Hello.jsx
@@ -1,13 +1,15 @@
 import './Hello.css';
 
 
-function Hello(){
+function Hello({name,place,skills , skills2}){
+    const list = skills.map((ele)=> <li> {ele} </li> ) ;
+    
     return <>
-        <h1 className="Hello">Hello world</h1>
-        <h1 className="Hello">Hello world</h1>
-        <h1 className="Hello">Hello world</h1>
-        <h1 className="Hello">Hello world</h1>
+        <h1 className="Hello">Hello my name is {name}, I'm from {place}.
+        skills:{list} obj:{skills2}
+        </h1>
+       
     </>
-}
+} 
 
 export default Hello;
