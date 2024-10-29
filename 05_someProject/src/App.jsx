e36e12727f720343889fcc4e97696c34b097af3b
vikramChaurasiya <bhartivikram633@gmail.com>
2024-10-29 12:33:14 +0530

    bg changer

---
+++ 05_someProject/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import Bgchangerpr from "./Bgchangerpr";
+function App() {
+  // const [color, setColor] = useState("blue");
+
+  return (
+    <div className="w-full h-screen duration-200"
+    > 
+      <Bgchangerpr/>
+    </div>
+  );
+}
+
+export default App;
