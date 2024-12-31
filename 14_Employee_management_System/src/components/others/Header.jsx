be4e32c61e61aa031628e187c39a45b867fcbada
vikramChaurasiya <bhartivikram633@gmail.com>
2024-12-31 22:36:50 +0530

    Add some function

--- 14_Employee_management_System/src/components/others/Header.jsx
+++ 14_Employee_management_System/src/components/others/Header.jsx
@@ -13,7 +13,7 @@ export default function Header(props){
 
     return(
         <div className='flex items-end justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /> <span className="text-3xl font-semibold "> Vikram👋</span></h1>
+            <h1 className='text-2xl font-medium'>Hello <br /> <span className="text-3xl font-semibold "> UserName👋</span></h1>
             <button onClick={logOutUser} className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm">Logout</button>
         </div>
     )
